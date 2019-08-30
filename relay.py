50524ef60f644a520fae8e418b6df6b5d172c279
Répás Zoltán <repas.zoltann@gmail.com>
2019-08-30 08:48:05 +0200

    Update relay.py

--- relay.py
+++ relay.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
-pin = 18 #Valasztott pin
+pin = 18 #Pin number
 
 GPIO.setup(pin, GPIO.OUT)
 def setPin(mode):
@@ -17,7 +17,7 @@ def pinOFF():
     return()
 try:
         pinON()
-        sleep(3)
+        sleep(3) #Duration (second)
         pinOFF()
 
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
